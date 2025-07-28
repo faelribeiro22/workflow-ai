ae44c96dced490b0d4a9be1f52cfe6c00a226a70
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-07-28 06:24:02 -0300

    feat: first request to openai api

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_BASE = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
+
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+        Você é um expert em mídias sociais, especialmente no Twitter (X).
+
+        Sua tarefa é gerar um post para o Twitter (X) com base na entrada do usuário.
+        O post deve ser curto, direto e engajador, com no máximo 280 caracteres.
+
+        Mantenha o tom amigável e profissional, evitando jargões técnicos desnecessários.
+
+        Aqui está a entrada do usuário:
+        <topic>
+        {topic}
+        <topic>
+"""
+    payload = {
+        "model": "gpt-4o",
+        "input": prompt,
+    }
+    
+    response = requests.post(
+        {OPENAI_API_BASE},
+        json=payload,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {OPENAI_API_KEY}"}
+    )
+
+    response_text = response.json().get("output", [{}])[0].get("content", "")[0].get("text", "")
+
+    print(response.json())
+    response_text = response.json().get("response", "")
+
+    return response_text
+
+
+def main():
+    # user input => AI (LLM) to generate X post => output post
+    
+    usr_input = input("What should the post be about? ")
+    x_post = generate_x_post(usr_input)
+    print("Generated X post:")
+    print(x_post)
+
+
+
+
+if __name__ == "__main__":
+    main()
